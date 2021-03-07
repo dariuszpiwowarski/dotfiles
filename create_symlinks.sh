3d0bf5bd1202e67b1f331b771c7ee0e06c022777
Dariusz Piwowarski <piwowarskidariusz@gmail.com>
2021-03-07 21:53:00 +0100

    Auto install vim plugins

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/bash
 
 dir=$(pwd)
-for file in $pwd.*rc
+for file in $pwd*rc
 do
 	if [[ -f $file ]]; then
-		ln -s $dir/$file ~/$file
+		ln -s $dir/$file ~/.$file
 	fi
 done
+
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# And install vim plugins
+vim +PluginInstall +qall
+
